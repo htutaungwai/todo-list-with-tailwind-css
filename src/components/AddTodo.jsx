56ecdb8acb1c77c87b32905952c6c3852c6db133
Htut Aung Wai <defo19987@gmail.com>
2023-09-19 15:05:15 +0700

    merged develop branch to main

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import TodoForm from "./TodoForm";
 import { motion } from "framer-motion";
 import { GrFormClose } from "react-icons/gr";
 
 const addTodo = ({ showAddTodo, setShowAddTodo, todos, setTodos }) => {
+  function useOutsideDivCloser(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowAddTodo(false);
+        }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  const wrapperRef = useRef(HTMLElement || null);
+  useOutsideDivCloser(wrapperRef);
   return (
     <motion.div
       initial={{ opacity: 0, opacity: 0.1 }}
@@ -12,14 +29,9 @@ const addTodo = ({ showAddTodo, setShowAddTodo, todos, setTodos }) => {
       transition={{ duration: 0.5 }}
       className="absolute   w-screen h-screen duration-300 ease-in-out delay-200 transition-all backdrop-brightness-50 flex justify-center items-center"
     >
+      <div className=" w-full h-full absolute"></div>
       <div
-        className=" w-full h-full absolute"
-        id="outside"
-        onClick={(e) => {
-          if (e.target.id === "outside") setShowAddTodo(false);
-        }}
-      ></div>
-      <div
+        ref={wrapperRef}
         id="inside"
         className="bg-zinc-50 w-5/6 h-2/3 lg:w-1/3 relative pt-10 "
       >
