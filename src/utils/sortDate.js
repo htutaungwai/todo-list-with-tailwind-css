9ca042be816f9fa1b6469a3b8adfd88b5705d185
Htut Aung Wai <defo19987@gmail.com>
2023-10-01 20:16:59 +0700

    added date sorting function

--- src/utils/sortDate.js
+++ src/utils/sortDate.js
@@ -5,8 +5,13 @@ dates = [
   { id: 4, date: "Sun Oct 08 2023 00:00:00 GMT+0700 (Indochina Time)" },
   { id: 5, date: "Mon Oct 09 2023 00:00:00 GMT+0700 (Indochina Time)" },
   { id: 6, date: "Tue Oct 10 2023 00:00:00 GMT+0700 (Indochina Time)" },
+  { id: 7, date: "Thu Jan 04 2024 00:00:00 GMT+0700 (Indochina Time)" },
 ];
 
-dates.forEach((date) => {
-  console.log(Date.parse(date.date).getDate());
+dates.forEach(({ date }) => {
+  const exampleDate = new Date(date);
+  const year = exampleDate.getFullYear();
+  // sort by year
 });
+
+console.log(sortedByYear);
