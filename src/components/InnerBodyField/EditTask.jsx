6e5d1d373052d9c0de17f58fc1654487a4d16279
Htut Aung Wai <defo19987@gmail.com>
2023-12-03 07:08:22 +0700

    layouted edit task section

--- src/components/InnerBodyField/EditTask.jsx
+++ src/components/InnerBodyField/EditTask.jsx
@@ -16,6 +16,10 @@ import { revealEditPage } from "../../features/showPagesSlice/revealSlice";
 // COMPONENTS
 import MenuBar from "../Tiptap/MenuBar";
 
+// ICONS
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+
 const EditWeb = () => {
   const dispatch = useDispatch();
   const { title, description, checked, dateCreated, id, content } = useSelector(
@@ -38,15 +42,34 @@ const EditWeb = () => {
 
   return (
     <div className="w-full h-full max-h-full overflow-x-hidden overflow-y-scroll bg-zinc-200 md:bg-zinc-50 absolute top-0 md:block md:relative p-4 z-20 ">
-      <textarea
-        type="text title"
-        className="w-full rounded-md text-2xl sourceCode p-2 font-semibold  h-24 resize-none"
-        ref={titleRef}
-        value={title}
-        onChange={(e) => {
-          onChangeHandler(e.target.value, "TITLE");
-        }}
-      />
+      <div className="relative bg-zinc-100 w-full  pt-10">
+        <button
+          onClick={() => {
+            dispatch(revealEditPage(false));
+          }}
+          className="absolute top-0 right-0"
+        >
+          <IoCloseSharp color="red" className="text-3xl font-extrabold" />
+        </button>
+      </div>
+      <div className=" mt-5 relative">
+        <div className="absolute -top-3 -left-2 text-xl ">
+          <FaQuoteLeft />
+        </div>
+
+        <div className="absolute bottom-0 right-1 text-xl ">
+          <FaQuoteRight />
+        </div>
+        <textarea
+          type="text title"
+          className="w-full rounded-md text-2xl sourceCode p-2 font-semibold  h-24 resize-none before:content-['hello-world'] "
+          ref={titleRef}
+          value={title}
+          onChange={(e) => {
+            onChangeHandler(e.target.value, "TITLE");
+          }}
+        />
+      </div>
 
       <DateTimePicker
         value={new Date(dateCreated)}
@@ -58,17 +81,8 @@ const EditWeb = () => {
         }}
       />
 
-      <button
-        onClick={() => {
-          dispatch(revealEditPage(false));
-        }}
-      >
-        X
-      </button>
+      <br />
 
-      <br />
-      <br />
-      <br />
       <br />
 
       <MenuBar />
