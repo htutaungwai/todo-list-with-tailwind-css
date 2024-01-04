c6c558db04973c4c900fc717c9597cdc31f9e4b5
Htut Aung Wai <defo19987@gmail.com>
2024-01-04 16:37:56 +0700

    fixed SideBar darktheme

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -33,6 +33,30 @@ function SideBar() {
   // THEME
   const { mood } = useSelector((state) => state.theme);
 
+  // TASK SECTIONS
+  const sections = [
+    {
+      name: "completed",
+      icon: <IoCheckmarkCircleSharp className="text-green-400" />,
+      href: "/tasks/completed",
+    },
+    {
+      name: "deleted",
+      icon: <IoTrash className="text-red-600" />,
+      href: "/tasks/deleted",
+    },
+    {
+      name: "favourites",
+      icon: <IoSparkles className="text-yellow-400" />,
+      href: "/tasks/favourites",
+    },
+    {
+      name: "ongoing",
+      icon: <IoCellular className="text-blue-700" />,
+      href: "/tasks/ongoing",
+    },
+  ];
+
   useEffect(() => {
     if (sideBarState) {
       open();
@@ -50,9 +74,17 @@ function SideBar() {
         }}
         withCloseButton={false}
         position="right"
-        className="bg-red-400"
+        styles={{
+          content: {
+            backgroundColor: mood === "light" ? "Window" : "ThreeDDarkShadow",
+          },
+        }}
       >
-        <div className="flex flex-row items-center justify-between font-sans">
+        <div
+          className={`${
+            mood === "light" ? "text-black" : "text-white"
+          } flex flex-row items-center justify-between font-sans `}
+        >
           <div className="flex flex-row gap-4 items-center">
             <span className=" text-4xl">
               <IoPersonCircleSharp />
@@ -70,34 +102,32 @@ function SideBar() {
           />
         </div>
 
-        <div className="">
+        <div className={`${mood === "light" ? "text-black" : "text-white"} `}>
           <ul className="w-full  h-auto flex flex-col gap-1 pt-5 ">
-            <li className="w-full  py-1 rounded-sm cursor-pointer px-2 hover:backdrop-blur-md hover:bg-red-50 ease-in-out transition-all flex flex-row  items-center gap-2">
-              <IoCheckmarkCircleSharp className="text-green-400" />
-              <h5 className="text-sm">Completed</h5>
-            </li>
-
-            <li className="w-full  py-1 rounded-sm cursor-pointer px-2 hover:backdrop-blur-md hover:bg-red-50 ease-in-out transition-all flex flex-row  items-center gap-2">
-              <IoTrash className="text-red-600" />
-              <h5 className="text-sm">Deleted</h5>
-            </li>
-
-            <li className="w-full  py-1 rounded-sm cursor-pointer px-2 hover:backdrop-blur-md hover:bg-red-50 ease-in-out transition-all flex flex-row  items-center gap-2">
-              <IoSparkles className="text-yellow-400" />
-              <h5 className="text-sm">Favourites</h5>
-            </li>
-
-            <li className="w-full  py-1 rounded-sm cursor-pointer px-2 hover:backdrop-blur-md hover:bg-red-50 ease-in-out transition-all flex flex-row  items-center gap-2">
-              <IoCellular className="text-blue-700" />
-              <h5 className="text-sm">Ongoing</h5>
-            </li>
-
-            <div className="w-full bg-black h-[1px]"></div>
-
-            <button className="mt-3 w-full  py-1 rounded-md cursor-pointer px-2 hover:backdrop-blur-md bg-red-500 ease-in-out transition-all  text-white poppins">
-              Sign Out
-            </button>
+            {sections.map((section) => {
+              return (
+                <li
+                  className={`${
+                    mood === "light" ? "hover:bg-red-50" : "hover:bg-slate-800"
+                  } w-full  py-1 rounded-sm cursor-pointer px-2 hover:backdrop-blur-md  ease-in-out transition-all flex flex-row  items-center gap-2`}
+                >
+                  {section.icon}
+                  <h5 className="text-sm capitalize">{section.name}</h5>
+                </li>
+              );
+            })}
           </ul>
+
+          {/* DIVISION LINE */}
+          <div
+            className={`${
+              mood === "light" ? "bg-black" : "bg-white"
+            } w-full h-[1px] my-2`}
+          ></div>
+
+          <button className="mt-3 w-full  py-1 rounded-md cursor-pointer px-2 hover:backdrop-blur-md bg-red-500 ease-in-out transition-all  text-white poppins">
+            Sign Out
+          </button>
         </div>
       </Drawer>
     </>
